3d6c9122caf42a1d2ff033694bee2fb5ea677107
おまつ先生 <33661309+omatztw@users.noreply.github.com>
2021-01-09 01:55:30 +0900

    Fix

--- db.go
+++ db.go
@@ -279,7 +279,7 @@ func MakeRandom() UserInfo {
 	user.Tone = random(-20, 20)
 	user.Intone = random(0, 4)
 	user.Threshold = random(0, 1)
-	user.AllPass = random(0, 0.3)
+	user.AllPass = 0
 	user.Volume = 1.0
 
 	return user
